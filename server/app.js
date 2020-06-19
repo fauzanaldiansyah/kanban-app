40660b287f333611f69c21c844095fe6b0208723
fauzanaldiansyah <fauzanaldiansyah@gmail.com>
2020-06-19 22:27:01 +0700

    Test3

--- server/app.js
+++ server/app.js
@@ -1,27 +1,25 @@
-require('dotenv').config()
+if(process.env.NODE_ENV === 'development'){
+  require('dotenv').config()
+  const morgan = require('morgan')
+}
 
 const express = require('express')
 const routes = require('./routes/index')
 const app = express()
 const errorHandler = require('./middlewares/errorHandler')
-const port = process.env.PORT 
+const port = process.env.PORT || 3000
 const cors = require('cors')
 
 app.use(cors())
+if(process.env.NODE_ENV === "development"){
+  app.use(morgan('dev'))
+}
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/', routes)
 app.use(errorHandler)
 
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-io.on('connection', () => { 
-    console.log('server connected ')
-});
-server.listen(3000, function() {
-    console.log('server is running');
-  });
 
-// app.listen(port, () => {
-//     console.log(`app listening in port ${port}`)
-// })
+app.listen(port, () => {
+    console.log(`app listening in port ${port}`)
+})
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router()
 const userroutes = require('./userroutes.js')
 const taskroutes = require('./taskroutes.js')
 
-router.get('/', (req,res)=>{
-    res.json({msg:'Hello KANBAN!'})
+router.get ('/', (req,res) => { 
+    res.json({msg:"Hello KANBAN!"})
 })
 
 router.use('/users', userroutes)
